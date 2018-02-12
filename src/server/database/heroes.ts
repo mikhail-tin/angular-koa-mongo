5a5bd161e06fdc7bfb11104b3f141845d3c6fc9f
Mikhail-Tuvykin <Mikhail.Tuvykin@returnonintelligence.com>
2018-02-12 16:57:47 +0300

    fixed routing / added button (go back) / minor changes

--- src/server/database/heroes.ts
+++ src/server/database/heroes.ts
@@ -1,7 +1,7 @@
 export default [
-    {id: 1, name:1},
-    {id: 2, name:2},
-    {id: 3, name:3},
-    {id: 4, name:4},
-    {id: 5, name:5}
+    {id: 1, name: 'Archy'},
+    {id: 2, name: 'One-Two(Wild Bunch)'},
+    {id: 3, name: 'Handsome Bob(Wild Bunch)'},
+    {id: 4, name: 'Mumbles(Wild Bunch)'},
+    {id: 5, name: 'Johnny'}
 ];
