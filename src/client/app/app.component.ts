3922d70be2f2f20235af9b6bedf1baac053d96b1
Mikhail-Tuvykin <Mikhail.Tuvykin@returnonintelligence.com>
2018-02-12 15:38:19 +0300

    added routing

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'App';
+  title: string = 'Tour of Heroes';
   
 }
