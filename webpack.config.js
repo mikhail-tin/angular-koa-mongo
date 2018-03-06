d5dda9e721c83dc6b0ca641e21e1daed82112330
Mikhail-Tuvykin <Mikhail.Tuvykin@returnonintelligence.com>
2018-03-06 10:28:07 +0300

    remove webpack from project (webpack in angular-cli still there)

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = env => {
-    /*if(env.NODE_ENV == 'prod') {
-        return require('./config/webpack.prod.js');
-    } else {*/
-        return require('./config/webpack.dev.js');
-    //}
-};
