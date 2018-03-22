e836b222250b6071ed4a7a89ec51f5e87509216b
Mikhail-Tuvykin <Mikhail.Tuvykin@returnonintelligence.com>
2018-03-22 09:48:49 +0300

    fixes

--- src/client/app/hero-detail/hero-detail.component.spec.ts
+++ src/client/app/hero-detail/hero-detail.component.spec.ts
@@ -5,20 +5,32 @@ import { NgReduxTestingModule, MockNgRedux } from '@angular-redux/store/lib/test
 import { HeroActions } from '../store/hero.actions';
 import { FormsModule } from '@angular/forms';
 
-import { RouterModule } from '@angular/router';
+import { RouterModule, ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Hero } from '../models/hero';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
+  let subStore = { 
+    getState: () => { 
+      return {  heroes: [{_id: '123', name: 'Hero1'},{_id: '234', name: 'Hero2'},{_id: '345', name: 'Hero3'}] };
+    }
+  };
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ HeroDetailComponent ],
       imports: [NgReduxTestingModule, FormsModule, RouterModule, RouterTestingModule],
-      providers: [HeroActions]
+      providers: [HeroActions, MockNgRedux, {provide: ActivatedRoute, useValue: { params: Observable.of({id: '123'}) }}
+    ]
     })
     .compileComponents();
+
+    spyOn(MockNgRedux.getInstance(), 'configureSubStore').and.returnValue(subStore);
   }));
 
   beforeEach(() => {
@@ -30,6 +42,8 @@ describe('HeroDetailComponent', () => {
   
 
   it('should create', () => {
+    
+
     expect(component).toBeTruthy();
   });
 });
